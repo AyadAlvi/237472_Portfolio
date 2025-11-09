1333d396b468e22a4a0deeee1ff9879a5cb273e8
AyadAlvi <ayadalvi1125@gmail.com>
2025-11-09 07:12:40 +0600

    Update app.js V3

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,6 @@
 ﻿
-// const API_BASE = 'http://localhost:5000';
-const API_BASE = 'https://craft-collective-api.onrender.com'; 
+const API_BASE = 'http://localhost:5000';
+// const API_BASE = 'https://craft-collective-api.onrender.com'; 
 
 const STORAGE_KEYS = {
   auth: 'craft-collective-auth',
@@ -1073,5 +1073,6 @@ function init() {
 }
 
 init();
+
 
 
